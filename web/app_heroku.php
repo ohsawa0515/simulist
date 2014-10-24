da20b0d0f3768bb6acc75e155fc6b7c3fcd3ddda
Shuichi Ohsawa <ohsawa0515@gmail.com>
2014-10-24 18:46:35 +0900

    Memcachedにセッションを格納するように変更

--- web/app_heroku.php
+++ web/app_heroku.php
@@ -3,6 +3,12 @@
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
 
+ini_set('session.save_handler', 'memcached');
+ini_set('session.save_path', 'PERSISTENT=pool ' . getenv('MEMCACHIER_SERVERS'));
+ini_set('Memcached.sess_binary', 1);
+ini_set('Memcached.sess_sasl_username', getenv('MEMCACHIER_USERNAME'));
+ini_set('Memcached.sess_sasl_password', getenv('MEMCACHIER_PASSWORD'));
+
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 //Debug::enable();
 
