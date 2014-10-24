b1f911e766817d7dab89330332b7a8aba55b90ab
Shuichi Ohsawa <ohsawa0515@gmail.com>
2014-10-24 19:26:42 +0900

    Memcachedのini_set()を修正

--- web/app_heroku.php
+++ web/app_heroku.php
@@ -5,9 +5,9 @@ use Symfony\Component\Debug\Debug;
 
 ini_set('session.save_handler', 'memcached');
 ini_set('session.save_path', 'PERSISTENT=pool ' . getenv('MEMCACHIER_SERVERS'));
-ini_set('Memcached.sess_binary', 1);
-ini_set('Memcached.sess_sasl_username', getenv('MEMCACHIER_USERNAME'));
-ini_set('Memcached.sess_sasl_password', getenv('MEMCACHIER_PASSWORD'));
+ini_set('memcached.sess_binary', 1);
+ini_set('memcached.sess_sasl_username', getenv('MEMCACHIER_USERNAME'));
+ini_set('memcached.sess_sasl_password', getenv('MEMCACHIER_PASSWORD'));
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 //Debug::enable();
